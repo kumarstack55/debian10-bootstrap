7dba7dc6bd62605bf61f457264caef38674186ba
kumarstack55 <kumarstack55@gmail.com>
2020-05-17 16:10:54 +0000

    Add git clone

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # debian10-bootstrap
+
+```
+curl -s https://raw.githubusercontent.com/kumarstack55/debian10-bootstrap/master/bootstrap.sh | bash
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,12 @@ set -eux
 sudo apt-get update
 
 type git >/dev/null 2>&1 || sudo apt install git -y
+
+cd $HOME
+if [[ ! -d $HOME/debian10-bootstrap ]]; then
+  git clone https://github.com/kumarstack55/debian10-bootstrap.git
+fi
+
 type ansible >/dev/null 2>&1 || sudo apt install ansible -y
 
 cd $HOME/debian10-bootstrap/playbooks
